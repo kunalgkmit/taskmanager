338eab781ccbe4f2ed10ecd4759d8241ae085402
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-12-26 14:56:26 +0530

    fix: remove redundant styles and add Home import file

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
-import {View, Text} from 'react-native'
-import Home from './src/screens/home/Home';
+import Home from './src/screens/home';
 
-export default function App(){
-  return(
-    <View style={{flex: 1}}>
-      <Home/>
-    </View>
-  );
+export default function App() {
+  return <Home />;
 }
