4ef4ccba722a6e3518fd27aa0ee680e04f3ad1b0
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-12-30 16:23:03 +0530

    feat: implement style on custom button

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -2,14 +2,16 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
     borderWidth: 1,
     borderRadius: 20,
     padding: 10,
     alignItems: 'center',
     margin: 3,
+    width: 130,
   },
   text: {
     fontWeight: 'bold',
+    color: '#7E60BF',
   },
 });
