08b8907add93f8692dd0418f4709287f23499e4e
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-12-31 15:01:05 +0530

    feat: implement safe area using useSafeAreaInsets

--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
 import { styles } from './styles';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function AppBar() {
+  const insets = useSafeAreaInsets();
   return (
-    <View style={styles.appBar}>
+    <View style={[styles.appBar, { paddingTop: insets.top }]}>
       <Text style={styles.appTitle}>Task Manager</Text>
     </View>
   );
--- src/components/appBar/styles.ts
+++ src/components/appBar/styles.ts
@@ -3,13 +3,12 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   appBar: {
     backgroundColor: '#2C2C2E',
-    width: 500,
-    height: 55,
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   appTitle: {
-    marginTop: 5,
+    marginBottom: 10,
     fontSize: 18,
     color: 'white',
     fontWeight: 'bold',
