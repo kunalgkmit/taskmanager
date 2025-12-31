d48d1c6e6e66dc296c57501d08f665098d5f22f4
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-12-31 17:22:03 +0530

    refactor: remove marginLeft from FAB

--- src/components/addTaskModalForm/AddTaskModalForm.tsx
+++ src/components/addTaskModalForm/AddTaskModalForm.tsx
@@ -1,5 +1,6 @@
 import { View } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { styles } from './styles';
 import { TaskModal } from '../taskModal';
 import { TaskForm } from '../taskForm';
 import { Task } from '../../types/type';
@@ -39,7 +40,7 @@ export default function AddTaskFormModal({
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TaskModal
         modalName={modalName}
         resetEditStates={resetEditStates}
--- src/components/floatingActionButton/styles.ts
+++ src/components/floatingActionButton/styles.ts
@@ -5,7 +5,7 @@ export const styles = StyleSheet.create({
   fab: {
     position: 'absolute',
     bottom: 24,
-    marginLeft: 115,
+    right: 24,
     width: 50,
     height: 50,
     borderRadius: 15,
@@ -13,6 +13,8 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     elevation: 6,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   image: { height: 30, width: 30 },
 });
