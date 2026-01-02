5dc5bf551ef08a275b9625f5ae1e30d5d8f20a3e
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-02 10:54:39 +0530

    refactor: make the type declarations globally available without imports

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,7 +1,13 @@
-export interface Task {
-  taskId: number;
-  title: string;
-  priority: string;
-  description: string;
-  status: boolean = false;
+declare global {
+  interface Task {
+    taskId: number;
+    title: string;
+    priority: string;
+    description: string;
+    status: boolean = false;
+  }
+
+  type ViewMode = 'none' | 'filter' | 'sort';
 }
+
+export {};
