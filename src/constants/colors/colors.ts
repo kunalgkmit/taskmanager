42039346e50f7dd9188b2bcb02641b6499a0d7ba
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-02 12:30:09 +0530

    refactor: remove border color from checkbox and fix color code

--- src/constants/colors/colors.ts
+++ src/constants/colors/colors.ts
@@ -2,8 +2,7 @@ export const COLORS = {
   primary: '#2C2C2E',
   secondary: '#FFFFFF',
   taskDescription: '#B0B0B0',
-  checkboxBorder: 'black',
-  error: 'red',
+  error: '#FF0000',
   checkBoxFillColor: '#78787bff',
   selectIcon: '#FFEF5F',
 };
